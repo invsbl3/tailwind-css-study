6b1d3aac595880a67e48940b51ec773eb9a3745a
unknown <luankevinfmb@gmail.com>
2024-10-05 21:13:20 -0400

    some extra info in layout studies

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TailWind CSS Guides
 
 This repository includes some simple but important Tailwind and CSS studies.
-On Chrome, remember to click `F12` to open the `dev console` and check the `html` rendered with `tailwind properties`.
+On Chrome, remember to click `F12` to open the `dev console` and check the `html` rendered with `tailwind properties` changing!!!
 
 Check the [`tailwind documentation`](https://tailwindcss.com/docs), it's very well done.
 
@@ -18,6 +18,8 @@ I started with `parent-elements` with `flex` and `grid` styles with `buttons` as
 - `align-items`, `align-content`, `align-self` and its behaviours
 
 ## Layout Patterns
+On Chrome, Press F12 (to show the dev mode), change the screen size and check the layout changes!!!!
+
 - There's a simple layout pattern used in [Tailwind Documentation Page](https://tailwindcss.com/docs) that I replied in this mini-lesson trying to simplify and show dinamic code on screen.
 - This patterns consists in divide the content in 3 columns (left bar, content and documentation in the right), but in very-small screens just the two first colums appear stacked (the third one is hidden).
 
--- src/Lessons/LayoutPatterns/LayoutPatterns.tsx
+++ src/Lessons/LayoutPatterns/LayoutPatterns.tsx
@@ -21,6 +21,16 @@ const LayoutPatterns = () => {
 
     const smDimension = 640;
 
+    const rows = [];
+    for (let i = 0; i < 12; i++) {
+        // note: we are adding a key prop here to allow react to uniquely identify each
+        // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+        rows.push(<div key={i} className="col-span-1 min-h-[50px] rounded-lg border-gray-400 border-2 p-2 max-sm:hidden">
+            <div className="text-[10px] h-[90%] content-center ">col-span-1</div>
+        </div>);
+    }
+
+
 
     const cont = '<div className="m-4 grid sm:grid-cols-12 w-[90vw] gap-2">'
     const div1 = '<div className="bg-red-500 min-h-[100px] sm:col-span-2 rounded-lg"></div>';
@@ -40,6 +50,7 @@ const LayoutPatterns = () => {
         <div className="m-4"><strong>Container Code:</strong>                     {(windowDimensions.width < smDimension) ? "grid (no grid-cols, so only one col, so one element per line)" : "grid sm:grid-cols-12"}
         </div>
         <div className="m-4 grid sm:grid-cols-12 w-[90vw] gap-2">
+            {rows}
             <div className="bg-red-500 min-h-[100px] sm:col-span-2 rounded-lg p-4">
                 <div className="text-sm h-[90%] content-center">
                     {(windowDimensions.width < smDimension) ? "(no code, so take all width of column)" : "sm:cols-span-2"}
@@ -57,21 +68,25 @@ const LayoutPatterns = () => {
             </div>
         </div>
 
+        <div className="m-4 w-[90vw] grid grid-cols-1 sm:grid-cols-2">
+            <div className={"col-span-1 m-4 p-4 border-2 border-gray-400 rounded-lg".concat((windowDimensions.width < smDimension) ? "" : " border-transparent")}>
+                <h1>When in small screens (sm) or above:</h1>
+                <p>
+                    container is a grid with 12 spaces: "grid-cols-12"<br />
+                    first element takes 2 spaces: "col-span-2"<br />
+                    second element takes 8 spaces: "col-span-8" <br />
+                    third element takes the 2 avaiable spaces: "col-span-2" <br />
+                </p>
+            </div>
 
-        <div className="m-4">
-            <h1>When in small screens (sm) or above:</h1>
-            <p>
-                container is a grid with 12 spaces: "grid-cols-12"<br />
-                first element takes 2 spaces: "col-span-2"<br />
-                second element takes 8 spaces: "col-span-8" <br />
-                third element takes the 2 avaiable spaces: "col-span-2" <br />
-            </p>
 
-            <h1>When in very small screens (less than sm size):</h1>
-            <p> container is a grid (with no other commands, so the default is "grid-cols-1", or "one single column" which means "one element per line"<br />
-                third element is "hidden"</p>
+            <div className={"col-span-1 m-4 p-4 pt-0 border-2 border-gray-400 rounded-lg".concat((windowDimensions.width < smDimension) ? " border-transparent" : "")}>
+
+                <h1>When in very small screens (less than sm size):</h1>
+                <p> container is a grid (with no other commands, so the default is "grid-cols-1", or "one single column" which means "one element per line"<br />
+                    third element (in green) is "hidden"</p>
+            </div>
         </div>
-     
 
 
         <div className="m-4"><h1>The "entire code"</h1>        </div>
